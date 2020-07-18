a8cb72589d717b479cee720729461d0b7cc5299c
magicbart <magicbart@gmail.com>
2020-07-18 18:22:49 +0200

    Add function create article

--- Repository/ArticleRepository.php
+++ Repository/ArticleRepository.php
@@ -3,9 +3,26 @@
 namespace ProjetNormandie\ArticleBundle\Repository;
 
 use Doctrine\ORM\EntityRepository;
+use ProjetNormandie\ArticleBundle\Entity\Article;
 
 class ArticleRepository extends EntityRepository
 {
+    /**
+     * @param $data
+     * @throws \Doctrine\ORM\ORMException
+     */
+    public function create($data)
+    {
+        $article = new Article();
+        $article->translate('en', false)->setTitle($data['title']['en']);
+        $article->translate('fr', false)->setTitle($data['title']['fr']);
+        $article->translate('en', false)->setText($data['text']['en']);
+        $article->translate('fr', false)->setText($data['text']['fr']);
 
+        $article->setAuthor($data['author']);
+        $article->mergeNewTranslations();
 
+        $this->_em->persist($article);
+        $this->_em->flush();
+    }
 }
