fdade10b12b961745f341f9b8854a62d82b2b264
magicbart <magicbart@gmail.com>
2025-04-17 19:55:47 +0200

    Add SF7 support

--- src/Doctrine/TranslationExtension.php
+++ src/Doctrine/TranslationExtension.php
@@ -13,39 +13,22 @@ use ProjetNormandie\ArticleBundle\Entity\Article;
 
 final class TranslationExtension implements QueryCollectionExtensionInterface, QueryItemExtensionInterface
 {
-    /**
-     * @param QueryBuilder                $queryBuilder
-     * @param QueryNameGeneratorInterface $queryNameGenerator
-     * @param string                      $resourceClass
-     * @param Operation|null              $operation
-     * @param array<mixed>                $context
-     * @return void
-     */
     public function applyToCollection(
         QueryBuilder $queryBuilder,
         QueryNameGeneratorInterface $queryNameGenerator,
         string $resourceClass,
-        Operation $operation = null,
+        ?Operation $operation = null,
         array $context = []
     ): void {
         $this->addWhere($queryBuilder, $resourceClass);
     }
 
-    /**
-     * @param QueryBuilder                $queryBuilder
-     * @param QueryNameGeneratorInterface $queryNameGenerator
-     * @param string                      $resourceClass
-     * @param array<mixed>                $identifiers
-     * @param Operation|null              $operation
-     * @param array<mixed>                $context
-     * @return void
-     */
     public function applyToItem(
         QueryBuilder $queryBuilder,
         QueryNameGeneratorInterface $queryNameGenerator,
         string $resourceClass,
         array $identifiers,
-        Operation $operation = null,
+        ?Operation $operation = null,
         array $context = []
     ): void {
         $this->addWhere($queryBuilder, $resourceClass);
